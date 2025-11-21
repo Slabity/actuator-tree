a75f6745014f5f05da22ebf919083eb90d63de51
Tyler Slabinski <slabity@slabity.dev>
2025-11-21 14:32:01 -0500

    Initialize Nix Flake Lockfile

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
 
    toolchain = inputs.fenix.packages.${system}.fromToolchainFile {
      file = ./rust-toolchain.toml;
-     sha256 = "sha256-Yt/+VH2ih9dDq83dNv0C3MtxJpxxVzWIW+FF0e0aAAw=";
+     sha256 = "sha256-7QhnCbFSz5m7AoV8Gtj1pM7LlZXAuQCVs7vLkdjDaZQ=";
    };
  in
  {
